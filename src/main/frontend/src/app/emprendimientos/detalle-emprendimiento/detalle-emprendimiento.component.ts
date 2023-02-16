8e73aa44a994e121a34ad76c87629a8e12b49b70
joaquin.mendoza.so <joaquin.mendoza@softtek.com>
2023-02-15 23:12:15 -0300

    Se commitea avance en frontend

---
+++ src/main/frontend/src/app/emprendimientos/detalle-emprendimiento/detalle-emprendimiento.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-detalle-emprendimiento',
+  templateUrl: './detalle-emprendimiento.component.html',
+  styleUrls: ['./detalle-emprendimiento.component.css']
+})
+export class DetalleEmprendimientoComponent {
+
+}
---
+++ src/main/frontend/src/app/emprendimientos/emprendimientos.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-emprendimientos',
+  templateUrl: './emprendimientos.component.html',
+  styleUrls: ['./emprendimientos.component.css']
+})
+export class EmprendimientosComponent {
+
+}
