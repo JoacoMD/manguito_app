3ef854d94c4762e064c803640a089ff71d626bd6
Joaquin Mendoza Dib <mendozadibjoaquiin@gmail.com>
2023-05-22 13:37:56 -0300

    Fix ng build

--- frontend/src/env.d.ts
+++ frontend/src/env.d.ts
@@ -22,22 +22,22 @@ interface ImportMetaEnv {
  * @deprecated process.env usage 
  * prefer using meta.env
  * */
-declare var process: {
-  env: {
-    NG_APP_ENV: string;
-    NG_APP_API_URL: string;
-    [key: string]: any;
-  };
-};
+// declare var process: {
+//   env: {
+//     NG_APP_ENV: string;
+//     NG_APP_API_URL: string;
+//     [key: string]: any;
+//   };
+// };
 
 // If your project references @types/node directly (in you) or indirectly (as in RxJS < 7.6.0), 
 // you might need to use the following declaration merging.
-// declare namespace NodeJS {
-//   export interface ProcessEnv {
-//     readonly NG_APP_ENV: string;
-//     // Add your environment variables below
-//   }
-// }
+declare namespace NodeJS {
+  export interface ProcessEnv {
+    readonly NG_APP_API_URL: string;
+    // Add your environment variables below
+  }
+}
 
 // If you're using Angular Universal and process.env notation, you'll need to add the following to your tsconfig.server.json:
 /* In your tsconfig.server.json */
